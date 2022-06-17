d38b842b76e891b9eeb37eadadae4c051b182e2f
imMatheus <matheusmendesbarata03@gmail.com>
2022-06-17 18:28:12 +0200

    done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next'
+import { trpc } from '@/utils/trpc'
 
 const Home: NextPage = () => {
-    return <div className='bg-red-500'>Hello, world!</div>
+	const res = trpc.useQuery(['hello', { text: 'Elon Musk' }])
+
+	return <div className="bg-red-500">{res.data?.text}</div>
 }
 
 export default Home
